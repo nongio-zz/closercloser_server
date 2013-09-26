49e5d1604b8b85765ecc844b3ad3a177f955c94e
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2013-09-26 18:10:03 +0200

    new database model
    :from field
    :n which blob
    :time data time
    :ms current ms

--- migrations/create_coords_table.rb
+++ migrations/create_coords_table.rb
@@ -4,9 +4,12 @@ require '../db.rb'
 
 ActiveRecord::Schema.define do
 	create_table :coords do |table|
+		table.column :from, :string
+		table.column :n, :integer
 		table.column :x, :integer
 		table.column :y, :integer
-		table.column :time, :date
+		table.column :time, :datetime
+		table.column :ms, :integer
 		table.column :sent, :boolean
 	end
 end
