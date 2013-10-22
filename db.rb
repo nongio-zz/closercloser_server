3764c8efb5e0c4f071ab7e141591d58d62b8c476
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2013-10-22 23:59:25 +0200

    hoootfix

--- db.rb
+++ db.rb
@@ -5,8 +5,9 @@ require 'mysql'
 
 ActiveRecord::Base.establish_connection(
   adapter:  'mysql', # or 'postgresql' or 'sqlite3'
-  database: 'closercloser',
-  username: "riccardo"
+  database: 'riccardo_closercloser',
+  username: "closer",
+  password: "close"
 )
 
 class Coords < ActiveRecord::Base
